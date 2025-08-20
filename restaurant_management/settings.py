f27aafe8bd06c2a94c0ca2654f1ec0ef3638ee0c
Rujal <141760256+rujal2004@users.noreply.github.com>
2025-08-20 16:22:14 +0530

    31460

--- restaurant_management/settings.py
+++ restaurant_management/settings.py
@@ -126,4 +126,7 @@ STATIC_URL = 'static/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
+# Custom Project Settings
+RESTAURANT_NAME ="The Kitchen"
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
