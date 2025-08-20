741fc58c9723dc31b8410764a71a5d5352562c57
Rujal <141760256+rujal2004@users.noreply.github.com>
2025-08-20 16:24:34 +0530

    31460

--- products/views.py
+++ products/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from django.conf import settings
+
 
 from .models import Item
 from .serializers import ItemSerializer
@@ -9,6 +11,16 @@ from .serializers import ItemSerializer
 '''
 NOTE: Conside this as a reference and follow this same coding structure or format to work on you tasks
 '''
+
+
+def home(request):
+    context:{
+        "restaurant_name":settings.RESTAURANT_NAME
+
+    }
+    return render(request,"index.html",context)
+
+
 
 # Create your views here.
 class ItemView(APIView):
@@ -24,3 +36,4 @@ class ItemView(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+  
